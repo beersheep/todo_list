099a9b742a4ab634797f794641934d3c3ee55839
beersheep <beersheep0109@gmail.com>
2016-02-01 15:36:19 +0800

    a lot.. basically modals?

--- javascripts/todo.js
+++ javascripts/todo.js
@@ -2,6 +2,20 @@ var App = {
   $main: $("main"),
   $sidebar: $("sidebar"),
   $add_todo: $("#add_todo"),
+  todo_count: 0,
+  countTodo: function() {
+    var self = this,
+        undone_item = this.Todos.models.filter(function(model){
+          return !model.complete;
+        });
+
+    self.todo_count = undone_item.length;
+    this.renderTodoCount();
+  },
+  renderTodoCount: function() {
+    var self = this;
+    $(".all_todos").html(templates.all_todo_count(App))
+  },
   renderAddForm: function(e) {
     e.preventDefault();
 
@@ -11,6 +25,16 @@ var App = {
   removeAddTodoForm: function() {
     this.$add_todo.html(templates.remove_todo_form);
     this.$add_todo.on("click", this.renderAddForm.bind(this));
+  },
+  removeItem: function(e) {
+    e.preventDefault();
+    var $this = $(e.target),
+        $li = $this.closest("li"),
+        idx = +$this.closest("li").attr("id"),
+        model = App.Todos.get(idx);
+
+    App.Todos.remove(model);
+    App.countTodo();
   },
   addTodoItem: function(e) {
     e.preventDefault();
@@ -26,10 +50,69 @@ var App = {
     });
 
     view = new this.TodoView(model);
-    view.$el.appendTo("#todo_items")
+    view.$el.appendTo("#todo_items");
 
     this.removeAddTodoForm();
+    this.countTodo();
 
+  },
+  showModal: function(e) {
+    e.preventDefault();
+
+    var $this = $(e.target),
+        $li = $this.closest("li"), 
+        $modal = $li.find(".modal"),
+        $modals = $li.find(".modal, .modal_layer"),
+        idx = +$li.attr("id"),
+        model = App.Todos.get(idx);
+
+    $modal.css({
+      top: $(window).scrollTop() + 100
+    });
+
+    $modals.fadeIn(300);
+
+    $modal.find(".complete").on("click", App.completeTask.bind(App));
+    $modal.find("form").on("submit", App.updateModel.bind(App));
+  },
+  updateModel: function(e) {
+    e.preventDefault();
+    var $form = $(".modal").find("form:visible"),
+        $li = $form.closest("li"), 
+        data = $form.serializeArray(),
+        idx = +$li.attr("id"),
+        model = this.Todos.get(idx),
+        temp = {};
+
+    data.forEach(function(obj){
+      temp[obj.name] = obj.value;
+    });
+
+    if (temp.due_day && temp.due_year && temp.due_day) {
+      temp.date = new Date(temp.due_year, (temp.due_month - 1), temp.due_day);
+      for (prop in temp) {
+        if (prop.startsWith("due")) {
+          delete temp[prop];
+        }
+      }
+    }
+
+    for (prop in temp) {
+      model.set(prop, temp[prop]);
+    }
+
+    console.log(model)
+
+    App.closeModal();
+  },
+  completeTask: function(e) {
+    e.preventDefault();
+
+    this.markDone(e);
+    this.closeModal();
+  },
+  closeModal: function() {
+    $(".modal, .modal_layer").fadeOut(300);
   },
   markDone: function(e) {
     e.preventDefault();
@@ -40,22 +123,34 @@ var App = {
 
     model.complete = !model.complete; 
 
+    App.countTodo();
     $li.toggleClass("finished");
 
   },
   bindEvent: function() {
     this.$add_todo.on("click", this.renderAddForm.bind(this));
-    this.$main.on("submit", "form", this.addTodoItem.bind(this));
+    this.$main.on("submit", "#add_item", this.addTodoItem.bind(this));
   },
   init: function() {
     this.bindEvent();
+    this.renderTodoCount();
+
   }
 };
 
-App.Todo = new ModelConstructor();
+
+App.Todo = new ModelConstructor({
+  // change: function() {
+  //   localStorage.setItem("todo_items", App.Todos.models);
+  // }
+});
+
 App.TodosConstructor = new CollectionConstructor();
 
 App.Todos = new App.TodosConstructor(App.Todo);
+
+
+App.countTodo = new ModelConstructor();
 
 var templates = {};
 
@@ -69,8 +164,11 @@ App.TodoView = new ViewConstructor({
   tag_name: "li", 
   template: templates.todo,
   events: {
-    "click .todo": App.markDone
+    "click .todo": App.showModal,
+    "click .delete_item": App.removeItem
   }
 });
+
+
 
 App.init();
